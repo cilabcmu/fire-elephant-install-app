029d77ff49e6f49bcf922ba7e765d45db2ce554b
Pannawit Panwong <pannawit.code@gmail.com>
2023-11-09 16:12:30 +0700

    remove unused code

--- src/hooks/useDrawer.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export const useDrawer = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const onChangeOpen = (value?: boolean) => (value ? setIsOpen(value) : setIsOpen(!isOpen));
-
-  return [isOpen, onChangeOpen] as const;
-};
